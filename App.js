a3d36ce0c62163e4c07f0ec2a332d95c779d362e
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-04-12 07:46:21 -0300

    comentários

--- App.js
+++ App.js
@@ -6,23 +6,27 @@ import BtnComponent from "./src/Components/btnComponent";
 import styles from "./src/Styles/styles";
 
 export default function App() {
-  const [celsius, setCelsius] = useState("");
-  const [modalVisible, setModalVisible] = useState(false);
-  const [fahrenheitValue, setFahrenheit] = useState("");
+  const [celsius, setCelsius] = useState(""); // Estado para armazenar o valor em Celsius
+  const [modalVisible, setModalVisible] = useState(false); // Estado para controlar a visibilidade do modal
+  const [fahrenheitValue, setFahrenheit] = useState(""); // Estado para armazenar o valor convertido para Fahrenheit
 
+  // Função para calcular o valor em Fahrenheit a partir do valor em Celsius
   const calcular = () => {
-    const fahrenheitValue = (celsius * 9) / 5 + 32;
+    const fahrenheitValue = (celsius * 9) / 5 + 32; // Fórmula de conversão de Celsius para Fahrenheit
+    // Verifica se o campo Celsius está vazio
     if (celsius === "") {
-      alert("Preencha todos os campos!");
+      alert("Preencha todos os campos!"); // Mostra um alerta se o campo estiver vazio
     } else {
-      setFahrenheit(fahrenheitValue);
-      console.log(fahrenheitValue);
-      setModalVisible(true);
+      setFahrenheit(fahrenheitValue); // Define o valor convertido para Fahrenheit no estado
+      console.log(fahrenheitValue); // Exibe o valor convertido no console
+      setModalVisible(true); // Torna o modal visível
     }
   };
+
+  // Função para fechar o modal e limpar o campo Celsius
   const fechar = () => {
-    setCelsius("");
-    setModalVisible(false);
+    setCelsius(""); // Limpa o campo Celsius
+    setModalVisible(false); // Torna o modal invisível
   };
 
   return (
@@ -45,7 +49,7 @@ export default function App() {
         <View style={styles.modalcontainer}>
           <TxtComponent
             styles={styles.btnText}
-            texto={`É Equivalente a ${fahrenheitValue} Fahrenheit`}
+            texto={`É Equivalente a ${fahrenheitValue} °F`}
           />
           <BtnComponent onPress={fechar}>
             <TxtComponent styles={styles.btnText} texto={"fechar"} />
